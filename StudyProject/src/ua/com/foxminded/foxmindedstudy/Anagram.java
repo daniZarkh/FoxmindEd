2e8d4cdd567f212c9757e067b249285b8e64543e
Valentin Rachkov <noslav7@gmail.com>
2021-08-31 22:00:52 +0500

    Task 1 - Anagram

---
+++ StudyProject/src/ua/com/foxminded/foxmindedstudy/Anagram.java
@@ -0,0 +1,28 @@
+package ua.com.foxminded.foxmindedstudy;
+
+public class Anagram {
+    public static String reverseString(String startString) {
+        char[] ifNotEmptyArray = new char[startString.length()];
+        char[] fromStartStringArray = startString.toCharArray();
+
+        if (startString.isEmpty()) {
+            return startString;
+        }
+
+        for (int i = 0; i < startString.length(); i++) {
+            while (Character.isLetter(fromStartStringArray[i])
+                    == Character.isLetter(fromStartStringArray[fromStartStringArray.length - 1 - i])) {
+                char temp = fromStartStringArray[i];
+                fromStartStringArray[i] = fromStartStringArray[fromStartStringArray.length - 1 - i];
+                fromStartStringArray[fromStartStringArray.length - 1 - i] = temp;
+            }
+        }
+
+        return fromStartStringArray.toString()
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "")
+                .replace(" ", "");
+    }
+}
+
