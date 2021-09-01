4029ca77595680ee50b59cf76bd2982d768e5f71
noslav7 <noslav7@gmail.com>
2021-09-01 10:37:00 +0500

    task 1 - Anagram

---
+++ StudyProject/src/ua/com/foxminded/foxmindedstudy/Main.java
@@ -0,0 +1,19 @@
+package ua.com.foxminded.foxmindedstudy;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String startString;
+
+        System.out.println("Enter some text: ");
+        Scanner scanner = new Scanner(System.in);
+        startString = scanner.nextLine();
+        scanner.close();
+        String[] words = startString.split("\\s+");
+        for (String word : words) {
+            String reversed = Anagram.reverseString(word);
+            System.out.println("The reversed string is: " + reversed);
+        }
+    }
+}
